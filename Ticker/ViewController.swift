f473909c491f3a5929bba45cce71c572573f8385
Samuel Beek <ik@samuelbeek.com>
2015-03-05 09:00:17 +0100

    Initial Commit

---
+++ Ticker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ticker
+//
+//  Created by Samuel Beek on 05-03-15.
+//  Copyright (c) 2015 Tosti. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
